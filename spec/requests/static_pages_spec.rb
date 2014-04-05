a707dd2edf116751797c2bf1b742bdfc77e4f716
jose antonio rodriguez leandro <alu0100696691@ull.edu.es>
2014-04-05 12:46:32 +0100

    creo test para la pagina de registro de usuarios con el controlador users

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,12 +2,22 @@ require 'spec_helper'
 
 describe "Static pages" do
 
-  describe "Log in" do
+  	describe "Log in" do
 
-    it "should have the content 'Registro'" do
-      visit '/static_pages/login'
-      expect(page).to have_content('Registro')
-    end
-  end
+    		it "should have the content 'Registro'" do
+      		visit '/static_pages/login'
+      		expect(page).to have_content('Registro')
+    		end
+  	end
+
+	describe "Registro" do
+
+    		it "should have the content 'REGISTRO'" do
+      		visit '/users/new'
+      		expect(page).to have_content('REGISTRO')
+    		end
+  	end
+	
+
 end
 
