2797141e7e985a0afc1bd3af4f34de63de181b11
jose antonio rodriguez leandro <alu0100696691@ull.edu.es>
2014-04-09 18:29:33 +0100

    arreglo fallo para dropdown en home y boton registro

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,10 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //  
-//= require bootstrap
 //= require jquery
 //= require jquery_ujs
+//= require bootstrap
+//= require bootstrap-dropdown
 //= require turbolinks
 //= require_tree .
  
