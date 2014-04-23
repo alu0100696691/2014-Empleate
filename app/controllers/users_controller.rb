bb6eea7b468f7723b0b7a838f5a4f0a3580fec58
jose antonio rodriguez leandro <alu0100696691@ull.edu.es>
2014-04-23 10:30:30 +0100

    signup failure

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,22 @@
 class UsersController < ApplicationController
  
-  def show
-  @user = User.find(params[:id])
-  @title = @user.name
-  end
+  	def show
+  		@user = User.find(params[:id])
+  		@title = @user.name
+  	end
 
 
-  def new
-	@user = User.new
-  end
+  	def new
+		@user = User.new
+  	end
+
+	def create
+    		@user = User.new(params[:user])    # Not the final implementation!
+    		if @user.save
+      		# Handle a successful save.
+    		else
+      		render 'new'
+    		end
+  	end
+
 end
