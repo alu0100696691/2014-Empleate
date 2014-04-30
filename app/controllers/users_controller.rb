359e734ef80adbdde48cd26632ee4336a6070827
jose antonio rodriguez leandro <alu0100696691@ull.edu.es>
2014-04-30 18:33:51 +0100

    Test de sigin-signup, borrado de seccion(cokies)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,8 @@ class UsersController < ApplicationController
 	def create
     		@user = User.new(user_params)
     		if @user.save
-    			flash[:success] = "Bienvenido a la red social Empleate!"
+    			sign_in @user
+			flash[:success] = "Bienvenido a la red social Empleate!"
 			redirect_to @user
 		else
       			render 'new'
