be6f3426816ec63a13da242a6528d1e2b7e396b4
jose antonio rodriguez leandro <alu0100696691@ull.edu.es>
2014-05-06 18:51:29 +0100

    Test de validacion de contactos

--- spec/models/contacto_spec.rb
+++ spec/models/contacto_spec.rb
@@ -1,5 +1,18 @@
 require 'spec_helper'
 
 describe Contacto do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  before do
+    @contacto = Contacto.new(name: "Example User", email: "user@example.com", mensaje: "Example")                     
+  end
+
+  subject { @contacto }
+
+  it { should respond_to(:name) }
+  it { should respond_to(:email) }
+  it { should respond_to(:mensaje) }
+   
+  it { should be_valid }
+
+
 end
