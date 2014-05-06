2375cbb599fe5c3cb6ca6e79b1efef8e1cfbdb11
jose antonio rodriguez leandro <alu0100696691@ull.edu.es>
2014-05-06 15:15:07 +0100

    creo tabla contacto para guardar los mensajes de ususarios

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140430140121) do
+ActiveRecord::Schema.define(version: 20140506141350) do
+
+  create_table "contactos", force: true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.text     "mensaje"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "name"
