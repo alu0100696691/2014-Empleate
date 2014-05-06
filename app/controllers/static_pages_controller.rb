66a65dbd47d6bc00ed76732bd38ce01f24dc99a1
jose antonio rodriguez leandro <alu0100696691@ull.edu.es>
2014-05-06 15:40:21 +0100

    controlador para guardar los contactos

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -9,6 +9,18 @@ class StaticPagesController < ApplicationController
   end
 
   def contact
+	@contacto = Contacto.new(user_params)
+	if @contacto.save
+      		redirect_to home
+    	else
+      		render 'contacto'
+    	end
   end
+
+	private
+
+    	def user_params
+      		params.require(:contacto).permit(:name, :email, :mensaje)
+    	end
 end
 
