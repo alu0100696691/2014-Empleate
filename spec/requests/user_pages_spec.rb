b703d6a719605e7e824420a74012b5ce382bce30
jose antonio rodriguez leandro <alu0100696691@ull.edu.es>
2014-05-14 12:24:57 +0100

    Test microposts

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -54,11 +54,23 @@ describe "User pages" do
 
 	describe "profile page" do
     	let(:user) { FactoryGirl.create(:user) }
+#-----------------------10.16
+	let!(:m1) { FactoryGirl.create(:micropost, user: user, content: "Foo") }
+	let!(:m2) { FactoryGirl.create(:micropost, user: user, content: "Bar") }
+#-----------------------10.16
+
     	before { visit user_path(user) }
 
     		it { should have_content(user.name) }
     		#it { should have_title(user.name) }
-  	end
+#-----------------------10.16
+	describe "microposts" do
+      		it { should have_content(m1.content) }
+      		it { should have_content(m2.content) }
+      		it { should have_content(user.microposts.count) }
+    	end
+#-----------------------10.16  		
+	end
 
 
 	
