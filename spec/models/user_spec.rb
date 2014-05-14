0413c96f2024255aea38145f5157c941e79b329f
Sandro Joa Garrido <alu0100716061@ull.edu.es>
2014-05-14 12:01:20 +0100

    Cuando se borra un usuario se desvincula su micropost

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -110,12 +110,22 @@ describe User do
       			FactoryGirl.create(:micropost, user: @user, created_at: 1.day.ago)
     		end
     		let!(:newer_micropost) do
-      		FactoryGirl.create(:micropost, user: @user, created_at: 1.hour.ago)
+	      		FactoryGirl.create(:micropost, user: @user, created_at: 1.hour.ago)
     		end
 
     		it "should have the right microposts in the right order" do
       		expect(@user.microposts.to_a).to eq [newer_micropost, older_micropost]
-   		 end
-  	end
+		end
+
+ #-------- 10.12
+		it "should destroy associated microposts" do
+      			microposts = @user.microposts.to_a
+      			@user.destroy
+      			expect(microposts).not_to be_empty
+      			microposts.each do |micropost|
+        		expect(Micropost.where(id: micropost.id)).to be_empty
+      			end
+    		end
+	end
 #-------- 10.10
 end
