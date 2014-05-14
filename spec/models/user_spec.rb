fccbd0317dbeb87d761398422799d4c136e2e785
Sandro Joa Garrido <alu0100716061@ull.edu.es>
2014-05-14 11:54:14 +0100

    Test de asociaciones de micropost

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -102,4 +102,20 @@ describe User do
     		before { @user.save }
     		its(:remember_token) { should_not be_blank }
   	end	
+#-------- 10.10
+	describe "micropost associations" do
+
+    		before { @user.save }
+    		let!(:older_micropost) do
+      			FactoryGirl.create(:micropost, user: @user, created_at: 1.day.ago)
+    		end
+    		let!(:newer_micropost) do
+      		FactoryGirl.create(:micropost, user: @user, created_at: 1.hour.ago)
+    		end
+
+    		it "should have the right microposts in the right order" do
+      		expect(@user.microposts.to_a).to eq [newer_micropost, older_micropost]
+   		 end
+  	end
+#-------- 10.10
 end
