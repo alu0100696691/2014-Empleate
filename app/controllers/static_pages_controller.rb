6fd6582df7c6158015df2a1870bd6b53fb2ca3f7
Noelia Rodriguez Martin <alu0100724622@ull.edu.es>
2014-05-23 12:46:53 +0100

    creo proto-feed, mostrar microposts en home segun usuario y test

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,12 @@
 class StaticPagesController < ApplicationController
+ 	
+ #------10.38
   def home
-    @micropost = current_user.microposts.build if signed_in?
-  end
+    if signed_in?
+      @micropost  = current_user.microposts.build
+      @feed_items = current_user.feed.paginate(page: params[:page])
+    end
+  end    
   
   def about
   end
